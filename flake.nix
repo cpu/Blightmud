19e2eaaa0249918d671272310459b2b0e15dd219
Daniel McCarney <daniel@binaryparadox.net>
2024-01-19 16:11:17 -0500

    wip: hacky proof of concept for Lua HTTP
    See https://gist.github.com/cpu/19dd22350b7acdcdd8cb506b0f5d5506

--- flake.nix
+++ flake.nix
@@ -13,9 +13,12 @@
         let
           cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 
-          runtimeDeps = with pkgs; [ alsa-lib openssl speechd ];
+          # A Lua 5.3 environment that includes the Lua http module
+          luaEnv = (pkgs.lua5_3.withPackages (ps: [ ps.http ]));
+
+          runtimeDeps = with pkgs; [ luaEnv alsa-lib openssl speechd ];
           buildDeps = with pkgs; [ pkg-config rustPlatform.bindgenHook ];
-          devDeps = with pkgs; [ gdb asciinema ];
+          devDeps = with pkgs; [ luaEnv gdb asciinema ];
 
           withFeatures = features: {
             inherit (cargoToml.package) name version;
@@ -25,12 +28,22 @@
             nativeBuildInputs = buildDeps;
             buildInputs = runtimeDeps;
             doCheck = false; # Some tests require networking
+
+            # Pass through a linker argument that instructs the linker to add
+            # all symbols, including those not referenced directly in the
+            # program. This is big hammer for a small nail: we really only
+            # want to preserve lua_* symbols...
+            RUSTFLAGS = "-C link-args=-rdynamic";
           };
 
           mkDevShell = rustc:
             pkgs.mkShell {
               shellHook = ''
                 export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
+
+                # Export some env vars for the Blightmud config script to use.
+                export BM_LUA_PATH="${luaEnv}/share/lua/5.3/?.lua;${luaEnv}/share/lua/5.3/?/init.lua"
+                export BM_LUA_CPATH="${luaEnv}/lib/lua/5.3/?.so"
               '';
               buildInputs = runtimeDeps;
               nativeBuildInputs = buildDeps ++ devDeps ++ [ rustc ];
